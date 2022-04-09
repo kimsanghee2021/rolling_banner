e55f7672f1ac02d0c1dd8933edc61f835075d208
kimsanghee <tkdgmltjddnr@naver.com>
2022-04-09 09:33:50 +0900

    데이터 바인딩

--- js/custom.js
+++ js/custom.js
@@ -2,22 +2,52 @@ const banner = document.querySelector('#banner');
 const list = banner.querySelector('.list');
 let num = 0;
 let timer = null;
-timer = setInterval(move, 10);
 
-list.addEventListener('mouseenter',function(){
-	clearInterval(timer);
-});
 
-list.addEventListener('mouseleave',function(){
-	timer = setInterval(move, 10);
-});
 
-function move() {
-	if (num < -240) {
-		num = 0;
-		list.append(list.firstElementChild);
-	} else {
-		num -= 2;
-	}
-	list.style.marginLeft = num + 'px';
+// timer = setInterval(move, 10);
+
+// list.addEventListener('mouseenter',()=>{clearInterval(timer)});
+
+// list.addEventListener('mouseleave',()=>{timer = setInterval(move, 10)});
+
+// function move() {
+// 	if (num < -240) {
+// 		num = 0;
+// 		list.append(list.firstElementChild);
+// 	} else {
+// 		num -= 2;
+// 	}
+// 	list.style.marginLeft = num + '00‚px';
+// }
+createList('data.json');
+function createList(url){
+	fetch(url)
+	.then((data)=> {
+		console.log(data);
+		return data.json();
+	})
+	.catch((err)=>{
+		console.log(err);
+		console.log('data호출에 실패했습니다.');
+	})
+	.then((json)=>{
+		console.log(json);
+		const arr = json.imgSrc;
+		let tags ='';
+		console.log(arr);
+
+		arr.forEach(function(el,idx){
+			tags +=`
+				<li>
+					<a href=${el.pic}>
+						<img src=${el.thumb}/>
+					</a>
+				</li>
+			`
+		});
+		list.innerHTML = tags;
+	});
+
+
 }
